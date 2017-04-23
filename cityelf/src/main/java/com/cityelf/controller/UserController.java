a71fff38b594c882ed3e8896357ca9f98b38a0c2
MikhailovOlegM <mikhailov.oleg.m@gmail.com>
2017-04-23 17:09:03 +0300

    main

--- cityelf/src/main/java/com/cityelf/controller/UserController.java
+++ cityelf/src/main/java/com/cityelf/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   @Autowired
+  private
   UserService us;
 
   @RequestMapping("/all")
--- cityelf/src/main/java/com/cityelf/service/UserService.java
+++ cityelf/src/main/java/com/cityelf/service/UserService.java
@@ -1,38 +1,20 @@
 package com.cityelf.service;
 
 import com.cityelf.model.User;
-import java.util.ArrayList;
+import com.cityelf.repository.UserRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-  private List<User> users = new ArrayList<>();
-
-  public UserService() {
-    User user = new User();
-    user.setFirstname("Oleg");
-    user.setLastname("Mikhailov");
-    user.setAdress("Lenina");
-    user.setEmail("norco_mount@mail.ru");
-    user.setPhone("0936436691");
-
-    users.add(user);
-
-    user = new User();
-    user.setFirstname("Sasha");
-    user.setLastname("Lusenko");
-    user.setAdress("Bunina");
-    user.setEmail("sasha_stim@mail.ru");
-    user.setPhone("063456321");
-
-    users.add(user);
-  }
-
+  @Autowired
+  private
+  UserRepository userRepository;
 
   public List<User> getAll() {
-    return users;
+    return userRepository.getUsers();
   }
 
 
