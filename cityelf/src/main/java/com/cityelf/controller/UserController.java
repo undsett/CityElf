28ce07e4c4ac6df60211960526686dd2dec24454
MikhailovOlegM <mikhailov.oleg.m@gmail.com>
2017-04-30 12:52:04 +0300

    Change text README and refactor styles

--- cityelf/src/main/java/com/cityelf/controller/UserController.java
+++ cityelf/src/main/java/com/cityelf/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cityelf.controller;
 
 import com.cityelf.model.User;
 import com.cityelf.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- cityelf/src/main/java/com/cityelf/service/UserService.java
+++ cityelf/src/main/java/com/cityelf/service/UserService.java
@@ -2,6 +2,7 @@ package com.cityelf.service;
 
 import com.cityelf.model.User;
 import com.cityelf.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
