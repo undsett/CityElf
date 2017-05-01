44a625e8dd1a45d33502404505fa6b88ec676000
YAROSLAV <salomatkin@gmail.com>
2017-05-01 22:11:19 +0300

    Profile REST service

--- cityelf/src/main/java/com/cityelf/repository/UserRepository.java
+++ cityelf/src/main/java/com/cityelf/repository/UserRepository.java
@@ -2,37 +2,101 @@ package com.cityelf.repository;
 
 import com.cityelf.model.User;
 
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
 
   private List<User> users = new ArrayList();
 
   public UserRepository() {
     User user = new User();
+    user.setId(1);
     user.setFirstname("Oleg");
     user.setLastname("Mikhailov");
-    user.setAdress("Lenina");
+    user.setAddress("Lenina");
     user.setEmail("norco_mount@mail.ru");
     user.setPhone("0936436691");
 
     users.add(user);
 
     user = new User();
+    user.setId(2);
     user.setFirstname("Sasha");
     user.setLastname("Lusenko");
-    user.setAdress("Bunina");
+    user.setAddress("Bunina");
     user.setEmail("sasha_asdasfas@mail.ru");
     user.setPhone("063456321");
+
+    users.add(user);
+
+    user = new User();
+    user.setId(3);
+    user.setFirstname("Vasya");
+    user.setLastname("Pupkin");
+    user.setAddress("Rishel'evskaya");
+    user.setEmail("vasya_pupkin@gmail.com");
+    user.setPhone("0735556644");
+
+    users.add(user);
+
+    user = new User();
+    user.setId(4);
+    user.setFirstname("Vlada");
+    user.setLastname("Testovaya");
+    user.setAddress("Levitana");
+    user.setEmail("vlada_test@rambler.ru");
+    user.setPhone("0487865432");
+
+    users.add(user);
+
+    user = new User();
+    user.setId(5);
+    user.setFirstname("Roma");
+    user.setLastname("Odesskij");
+    user.setAddress("Vodoprovodnaya");
+    user.setEmail("romaodessa@ukr.net");
+    user.setPhone("0946677321");
 
     users.add(user);
   }
 
   public List<User> getUsers() {
     return users;
+  }
+
+  public User getUser(long id) {
+    for (User user : users) {
+      if (user.getId() == id) {
+        return user;
+      }
+    }
+    return null;
+  }
+
+  public void addNewUser(User user) {
+    users.add(user);
+  }
+
+  public void updateUser(User user) {
+    long id = user.getId();
+    for (User userDb : users) {
+      if (userDb.getId() == id) {
+        users.set(users.indexOf(userDb), user);
+        break;
+      }
+    }
+  }
+
+  public void deleteUser(long id) {
+    for (User user : users) {
+      if (user.getId() == id) {
+        users.remove(user);
+        break;
+      }
+    }
   }
 }
