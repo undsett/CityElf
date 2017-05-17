b285d7fb176da3f8d9353efbbd718ebc21038ec1
Anton Panciuc <nikon.milnitsa@gmail.com>
2017-05-17 23:28:44 +0300

    S1T5: Spring Security module

--- cityelf/src/main/java/com/cityelf/controller/UserController.java
+++ cityelf/src/main/java/com/cityelf/controller/UserController.java
@@ -2,20 +2,43 @@ package com.cityelf.controller;
 
 import com.cityelf.model.User;
 import com.cityelf.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-public class UserController{
+public class UserController {
+
   @Autowired
-  private UserService us;
+  private UserService userService;
 
   @RequestMapping("/all")
-  public java.util.List<User> getAll()
-  {
-    return us.getAll();
+  public java.util.List<User> getAll() {
+    return userService.getAll();
   }
 
+  @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+  public User getUser(@PathVariable("userId") long id) {
+    return userService.getUser(id);
+  }
+
+  @RequestMapping(value = "/updateUser", method = RequestMethod.PUT)
+  public void updateUser(@RequestBody User user) {
+    userService.updateUser(user);
+  }
+
+  @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+  public void addNewUser(@RequestBody User user) {
+    userService.addNewUser(user);
+  }
+
+  @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE)
+  public void deleteUser(@PathVariable("userId") long id) {
+    userService.deleteUser(id);
+  }
 }
--- cityelf/src/main/java/com/cityelf/service/UserService.java
+++ cityelf/src/main/java/com/cityelf/service/UserService.java
@@ -5,15 +5,34 @@ import com.cityelf.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserService{
+public class UserService {
+
   @Autowired
   private UserRepository userRepository;
 
-  public UserService() {}
+  public UserService() {
+  }
 
-  public java.util.List<User> getAll()
-  {
+  public List<User> getAll() {
     return userRepository.getUsers();
+  }
+
+  public User getUser(long id) {
+    return userRepository.getUser(id);
+  }
+
+  public void addNewUser(User user) {
+    userRepository.addNewUser(user);
+  }
+
+  public void updateUser(User user) {
+    userRepository.updateUser(user);
+  }
+
+  public void deleteUser(long id) {
+    userRepository.deleteUser(id);
   }
 }
