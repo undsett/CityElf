37b697a1b98a21b6b1f3ee780943a692c7e6982d
Pasha <Dolbnev20@gmail.com>
2017-06-19 01:01:33 +0300

    S4T5: Create a rest service with CRUD operations for electricity shutdowns

---
+++ src/main/java/com/cityelf/model/Address.java
@@ -0,0 +1,42 @@
+package com.cityelf.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "street")
+    private String address;
+
+    public Address() {
+        this.address = "None";
+    }
+
+    public Address(String address) {
+        this.address = address;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
---
+++ src/main/java/com/cityelf/service/ElectricityForecastService.java
@@ -0,0 +1,83 @@
+package com.cityelf.service;
+
+
+import com.cityelf.exceptions.ForecastAlreadyExistsException;
+import com.cityelf.exceptions.ForecastNotFoundException;
+import com.cityelf.model.Address;
+import com.cityelf.model.ElectricityForecast;
+import com.cityelf.repository.ElectricityForecastRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class ElectricityForecastService {
+
+    @Autowired
+    private ElectricityForecastRepository electricityForecastRepository;
+
+    public ElectricityForecastService() {
+    }
+
+    public Iterable<ElectricityForecast> getAll(){
+        return electricityForecastRepository.findAll();
+    }
+
+    public ElectricityForecast getForecast(long id)throws ForecastNotFoundException {
+        ElectricityForecast forecast = electricityForecastRepository.findOne(id);
+        if(forecast == null){
+            throw new ForecastNotFoundException();
+        }
+        return forecast;
+    }
+
+    public ElectricityForecast getForecast(LocalDateTime startTime, String address) throws ForecastNotFoundException {
+        return electricityForecastRepository.findByStartAndAddress_Address(startTime, address)
+                .orElseThrow(ForecastNotFoundException::new);
+    }
+
+    public List<ElectricityForecast> getForecastsByTime(LocalDateTime startTime) {
+        return electricityForecastRepository.findByStart(startTime);
+    }
+
+    public void addNewElectricityForecast(ElectricityForecast forecast) throws ForecastAlreadyExistsException {
+        if(electricityForecastRepository.findByStartAndAddress(forecast.getStart(), forecast.getAddress()).isPresent()){
+            throw new ForecastAlreadyExistsException();
+        }
+        electricityForecastRepository.save(forecast);
+    }
+
+    public void updateElectricityForecast(ElectricityForecast forecast) throws ForecastNotFoundException {
+        if(!electricityForecastRepository.exists(forecast.getId())){
+            throw new ForecastNotFoundException();
+        }
+        electricityForecastRepository.save(forecast);
+    }
+
+    public void deleteElectricityForecast(ElectricityForecast forecast) throws ForecastNotFoundException {
+        if(!electricityForecastRepository.exists(forecast.getId())){
+            throw new ForecastNotFoundException();
+        }
+        electricityForecastRepository.delete(forecast);
+    }
+
+    public void deleteElectricityForecastsByTime(LocalDateTime startTime){
+        electricityForecastRepository.delete(getForecastsByTime(startTime));
+    }
+
+    public Set<Address> getAddressesByTime(LocalDateTime startTime) {
+        return getForecastsByTime(startTime).stream()
+                .map(ElectricityForecast::getAddress)
+                .collect(Collectors.toSet());
+    }
+
+    public List<ElectricityForecast> getCurrentElectricityForecasts(LocalDateTime checkedTime) {
+        return electricityForecastRepository
+                .findElectricityForecastsByStartLessThanEqualAndEstimatedStopGreaterThan(checkedTime, checkedTime);
+    }
+}
