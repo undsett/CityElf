5d28e3c735daf4dfd409636970aca8534cd0077e
DolbnevPasha <Dolbnev20@gmail.com>
2017-06-19 10:08:48 +0300

    S4T5: Create a rest service with CRUD operations for electricity shutdowns (#36)

--- src/main/java/com/cityelf/model/Address.java
+++ src/main/java/com/cityelf/model/Address.java
@@ -1,38 +1,41 @@
 package com.cityelf.model;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "addresses")
 public class Address {
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
 
-  @Id
-  private long id;
+    @Column(name = "street")
+    private String address;
 
-  @Column(name = "street")
-  private String address;
+    public Address() {
+        this.address = "None";
+    }
 
-  public Address() {
-    this.address = "None";
-  }
+    public Address(String address) {
+        this.address = address;
+    }
 
-  public Address(String address) {
-    this.address = address;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public long getId() {
-    return id;
-  }
+    public String getAddress() {
+        return address;
+    }
 
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
