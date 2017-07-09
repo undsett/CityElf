a173693476088a3c3db9b49d8d7c7a2a0e26f6f6
YAROSLAV <salomatkin@gmail.com>
2017-07-09 15:03:09 +0300

    fixing checkstyle errors

--- src/test/java/com/cityelf/ShutdownInfoControllerTest.java
+++ src/test/java/com/cityelf/ShutdownInfoControllerTest.java
@@ -1,11 +1,10 @@
 package com.cityelf;
 
-
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.cityelf.controller.ShutdownsInfoController;
@@ -15,20 +14,19 @@ import com.cityelf.service.ShutdownsInfoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = ShutdownsInfoController.class, secure = false)
@@ -74,21 +72,18 @@ public class ShutdownInfoControllerTest {
         .andExpect(status().isOk());
   }
 
-//  @Test
-//  public void getAllForecastsByTimeAndAddressShouldReturnJson() throws Exception {
-//    when(shutdownsInfoService.getAllForecasts(any(LocalDateTime.class), any(String.class)))
-//        .thenReturn(map);
-//
-//    Map<String, Object> expectedMap = new HashMap<>();
-//    expectedMap.put("Water", waterForecast);
-//
-//    mockMvc.perform(get("/allforecast/get")
-//        //.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//        .param("start", waterForecast.getStart().toString())
-//        .param("address", waterForecast.getAddress().getAddress()))
-//        .andDo(print())
-//        //.andExpect(content().contentType(MediaType.APPLICATION_FORM_URLENCODED))
-//        .andExpect(content().string(objectToJson(expectedMap)));
-//  }
+  @Test
+  public void getAllForecastsByTimeAndAddressShouldReturnJson() throws Exception {
+    when(shutdownsInfoService.getAllForecasts(any(LocalDateTime.class), any(String.class)))
+        .thenReturn(map);
 
+    Map<String, Object> expectedMap = new HashMap<>();
+    expectedMap.put("Water", waterForecast);
+
+    mockMvc.perform(get("/allforecasts/get")
+        .param("start", waterForecast.getStart().toString())
+        .param("address", waterForecast.getAddress().getAddress()))
+        .andDo(print())
+        .andExpect(content().string(objectToJson(expectedMap)));
+  }
 }
