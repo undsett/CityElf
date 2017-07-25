e94b30e9d90e50fbcc530562b7c4984594237747
DmitriyOsipov <osipov.dmal@gmail.com>
2017-07-26 00:22:15 +0300

    Fix: Gas Parser - added logging for some exceptions for warning, changed working logic, adapted for some variations of input information

--- src/main/java/com/cityelf/utils/ParserGas.java
+++ src/main/java/com/cityelf/utils/ParserGas.java
@@ -1,10 +1,13 @@
 package com.cityelf.utils;
 
 import com.cityelf.domain.ForcastData;
+import com.cityelf.exceptions.GasPageStructureChangedException;
 import com.cityelf.exceptions.ParserUnavailableException;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,6 +26,8 @@ public class ParserGas {
   @Autowired
   private GasForecaster gasForecaster;
 
+  private static final Logger logger = LoggerFactory.getLogger(AddressesFiller.class);
+
   private String newsTheme;
 
   private LocalDate dateToCheck;
@@ -37,15 +42,26 @@ public class ParserGas {
 
   public ParserGas() {
     this.dateToCheck = LocalDate.now();
-    this.newsTheme = "связи с производством";
+    this.newsTheme = "[(связи с производством)|(зв.язку з [(вико)|(рем)|(роб)])]";
   }
 
   public List<ForcastData> getForcastDataList() throws ParserUnavailableException {
     List<ForcastData> result = new ArrayList<>();
-    Elements neededNews = loader.getNeededNews(ParserGas.transformDate(dateToCheck),
-        newsTheme);
+    String dateToCheckRegex = String.format("((%s)|(%te %<tB %<tY))",
+        ParserGas.transformDate(dateToCheck), dateToCheck);
+    Elements neededNews = loader.getNeededNews(dateToCheckRegex, newsTheme);
+    boolean wasExceptions = false;
     for (Element pieceOfNews : neededNews) {
-      result.addAll(gasForecaster.getForecastData(pieceOfNews));
+      try {
+        result.addAll(gasForecaster.getForecastData(pieceOfNews, dateToCheck));
+      } catch (ParserUnavailableException exc) {
+        wasExceptions = true;
+        logger.error("There was an exception while gas page was parsing: " + exc.getMessage(), exc);
+      }
+    }
+    if (wasExceptions && result.isEmpty()) {
+      throw new GasPageStructureChangedException(
+          "There was an exceptions while gas page was parsing. Check log file");
     }
     return result;
   }
