128d68ab3baddc6523b4d977bcaeaf8ef6ae311c
Stanislav Parkhomenko <stanislav_parkhomenko@yahoo.com>
2017-08-13 09:31:15 +0300

    checkstyle fix

--- src/main/java/com/cityelf/service/ShutdownsInfoService.java
+++ src/main/java/com/cityelf/service/ShutdownsInfoService.java
@@ -13,8 +13,10 @@ import com.cityelf.repository.WaterForecastRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ShutdownsInfoService {
